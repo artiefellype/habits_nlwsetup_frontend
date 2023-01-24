a5d0245f2e90a49945f725fe8a6abd166a2c89ca
Arthur <arthurfellype21@gmail.com>
2023-01-24 17:20:28 -0300

    Adding modals and popover behavior

--- src/components/SummaryTable.tsx
+++ src/components/SummaryTable.tsx
@@ -6,7 +6,6 @@ const summaryDates = generateDatesFromYearBeginning();
 const minimumSummaryDatesSize = 18 * 7;
 const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length;
 
-console.log(summaryDates);
 export function SummaryTable() {
   return (
     <div className="w-full flex">
@@ -24,14 +23,24 @@ export function SummaryTable() {
       </div>
       <div className="grid grid-rows-7 grid-flow-col gap-3">
         {summaryDates.map((date, index) => {
-          return <HabitDay key={date.toString()} />;
+          return (
+            <HabitDay 
+              amount={5} 
+              amountCompleted={Math.round(Math.random() * 5)} 
+              key={date.toString()}
+            />
+          );
         })}
 
-        {amountOfDaysToFill > 0 && Array.from({length: amountOfDaysToFill}).map(()=>{
+        {amountOfDaysToFill > 0 &&
+          Array.from({ length: amountOfDaysToFill }).map((_, index) => {
             return (
-                <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed" ></div>
-            )
-        })}
+              <div
+                key={index}
+                className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
+              ></div>
+            );
+          })}
       </div>
     </div>
   );
